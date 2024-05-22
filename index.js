2906ddfd8b7f6ed0f7dda187f562efe61e8ea5ea
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2024-05-22 23:59:01 +0530

    learning

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("chai aur code")
+const express = require('express')
+const app = express()
+ 
+const port = 4500
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/twitter', (req,res) => {
+    res.send('kinston_18')
+})
+
+app.get('/login', (req,res) => {
+    res.send('<h1>Please login</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
